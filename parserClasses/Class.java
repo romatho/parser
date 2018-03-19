1ca728bd5b7cae858e8c599d7d07c87fd8e97cd5
Renaud <kidrenaud@hotmail.com>
2018-03-19 18:59:39 +0100

    laMeme

--- parserClasses/Class.java
+++ parserClasses/Class.java
@@ -1,11 +1,20 @@
 package parserClasses;
 
-public class Class {
+public class Class extends  Node{
 
     public String name;
     public String parentClass;
     public ParserArray<Field> myFields;
     public ParserArray<Method> myMethods;
+
+    public Class(int pColumn, int pLine, String pName, String pParentClass, ParserArray<Field> pMyFields, ParserArray<Method> pMyMethods)
+    {
+        super(pColumn,pLine);
+        name = pName;
+        parentClass = pParentClass;
+        myFields = pMyFields;
+        myMethods = pMyMethods;
+    }
 
     @Override
     public String toString()
--- parserClasses/Program.java
+++ parserClasses/Program.java
@@ -1,8 +1,14 @@
 package parserClasses;
 
-public class Program {
+public class Program extends Node {
 
     public ParserArray<Class> myClasses;
+
+    public Program(int pColumn, int pLine, ParserArray<Class> pMyClasses)
+    {
+        super(pColumn,pLine);
+        myClasses = pMyClasses;
+    }
 
     @Override
     public String toString()
@@ -10,5 +16,6 @@ public class Program {
         String toDisplay = "[";
         toDisplay += myClasses.toString();
         toDisplay += "]";
+        return toDisplay;
     }
 }
