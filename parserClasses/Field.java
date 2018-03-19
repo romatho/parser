41e0739a2a66bf1b4cf50facae405f28cc807080
Unknown <r.mathonet1@gmail.com>
2018-03-19 20:57:17 +0100

    puuuush

--- parserClasses/Field.java
+++ parserClasses/Field.java
@@ -1,9 +1,24 @@
 package parserClasses;
 
-public class Field {
+public class Field  extends Node {
+    private String identifier;
+    private Types type;
+    private Expressions expr;
 
-    public String getDisplayString()
-    {
 
+    public Field(int pColumn, int pLine, String identifier, Types type,Expressions expr) {
+        super(pColumn, pLine);
+        this.identifier = identifier;
+        this.type = type;
+        this.expr= expr;
     }
+
+    @Override
+    public String toString() {
+        if(expr==null)
+            return "Field(" + identifier + ", " + type.toString() +")";
+        else
+            return "Field(" + identifier + ", " + type.toString() +", "+ expr.toString()+")";
+    }
+
 }
