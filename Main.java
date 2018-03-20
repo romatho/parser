e6b69693f8e3739fe0ac86028ec1ca75e57490e1
Renaud <kidrenaud@hotmail.com>
2018-03-20 22:01:19 +0100

    lexer

--- Main.java
+++ Main.java
@@ -27,7 +27,7 @@ public class Main {
 
 
     				retrievedSym = lexer.next_token();
-					currentSym = new MySymb(retrievedSym,sym.terminalNames[retrievedSym.sym]);
+					currentSym = new MySymb(retrievedSym,(sym.terminalNames[retrievedSym.sym].replace("_", "-").toLowerCase());
     				currentSym.display();
     			}
     		}
